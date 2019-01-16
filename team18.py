c5b08b766c204e6d7f81333cd6d0276928ecea94
ltotheoman <224603@shakopeeschools.org>
2019-01-16 08:54:50 -0600

    Finished Strategy
    finished team strategy

--- team18.py
+++ team18.py
@@ -26,8 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    if len(my_history)==0:
+       return 'c'
+    elif their_history[-1]=='c':
+       return 'c'
+    else:
+       return 'b'
+    
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
@@ -53,7 +58,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
