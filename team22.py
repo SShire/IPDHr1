0f756f4ee7ce73c0d0c298e9f883ddf9672869ed
Will <228817@shakopeeschools.org>
2019-01-16 08:31:08 -0600

    Update team22.py
    Changed the team name and strategy. Also added the code for the C or B generation

--- team22.py
+++ team22.py
@@ -6,10 +6,13 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The Supreme Justices' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'The RNG GODS' # Only 10 chars displayed.
+strategy_name = 'RNG'
+strategy_description = '''this is random number generation from 
+1 - 1000 and finds the remander using 2 and if the said remainder is 
+0 betray and if not collude'''
+import random
+        
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -17,17 +20,12 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
+    if len(my_history) == 0:
+        return 'c'
+    elif random.randint(1,100)%2 == 0:
+        return 'b'
+    else:
+        return 'c'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
