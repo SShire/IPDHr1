5a7baf9307def756fee587df5c4b489e0112bcf0
Spencer <228997@shakopeeschools.org>
2019-01-17 08:24:40 -0600

    Update team19.py

--- team19.py
+++ team19.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = "Spencer's Team" # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Collude Or Betray Based On Score'
+strategy_description = 'If its score is greater than or equal to the other it colludes; if not it betrays.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,11 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if my_score >= their_score:
+        return 'c'
+    if their_score > my_score:
+        return 'b'
+    
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -53,7 +57,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
