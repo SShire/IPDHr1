e9ddb74c2378de8445474f0977e97340093f3ea1
kowsar <309637@shakopeeschools.org>
2019-01-18 08:38:47 -0600

    i change my name

--- team13.py
+++ team13.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Kowsar!' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Kowsar Gure team 13!' # Only 10 chars displayed.
+strategy_name = 'kowsG!'
+strategy_description = 'if my history-1 is and their history-1 is c then return c otherwest return b'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
