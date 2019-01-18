70b67d8faa28cfb50b46c2ba5439d75324aa374f
AGjacka <46531430+AGjacka@users.noreply.github.com>
2019-01-18 08:39:24 -0600

    Update team12.py

--- team12.py
+++ team12.py
@@ -8,6 +8,7 @@
 
 team_name = '12' # Only 10 chars displayed.
 strategy_name = 'AG jacka'
+strategy_description = 'If the other player has betrayed within last 10 rounds, betray them,else collude'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
