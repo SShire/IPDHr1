22e24eff0b5d48c2e228e26c76977e092b4adc06
ltotheoman <224603@shakopeeschools.org>
2019-01-24 08:32:46 -0600

    fuiudndni

--- team18.py
+++ team18.py
@@ -28,7 +28,7 @@ def move(my_history, their_history, my_score, their_score):
     
     if len(my_history)==0:
         return 'c'
-    elif 'b' in their_history[0:]:
+    elif 'b' in their_history[-5:]:
         return 'b'
     else:
         return 'c'
